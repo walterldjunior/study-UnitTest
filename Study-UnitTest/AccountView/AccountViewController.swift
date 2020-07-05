89d7a8129f3102da99cf3bcda01aceb6997423df
WalterLDJunior <walter.junior@meupag.com.br>
2020-07-05 17:58:02 -0300

    commit fix

---
+++ Study-UnitTest/AccountView/AccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AccountViewController.swift
+//  Study-UnitTest
+//
+//  Created by Walter Luis Deonísio Junior on 05/07/20.
+//  Copyright © 2020 Walter Luis Deonísio Junior. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+   var viewModel: AccountViewModelProtocol?
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           print("Up!")
+           
+           viewModel?.shouldRequestLocation(showAlert: {
+               print("Alerta")
+           }, askUserPermission: {
+               print("Get user Location")
+           }, completion: {
+               print("Completion")
+           })
+       }
+
+    @IBAction func loginButton(_ sender: Any) {
+        print("LoginButton")
+    }
+}
---
+++ Study-UnitTest/AccountView/AccountViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  AccountViewModel.swift
+//  Study-UnitTest
+//
+//  Created by Walter Luis Deonísio Junior on 05/07/20.
+//  Copyright © 2020 Walter Luis Deonísio Junior. All rights reserved.
+//
+
+import UIKit
+
+
+protocol AccountViewModelProtocol {
+    func loginButtonTap()
+    func shouldRequestLocation(showAlert: () -> (),
+                               askUserPermission: () -> (),
+                               completion: () -> ())
+}
+
+class AccountViewModel: AccountViewModelProtocol {
+    private let shouldserLocation: Bool
+    
+    init(shouldserLocation: Bool) {
+        self.shouldserLocation = shouldserLocation
+    }
+    
+    func loginButtonTap() {
+        print("Login Realizado com sucesso!")
+    }
+    
+    func shouldRequestLocation(showAlert: () -> (),
+                               askUserPermission: () -> (),
+                               completion: () -> ()) {
+        
+        if shouldserLocation {
+            askUserPermission()
+            completion()
+        }
+        
+        showAlert()
+        completion()
+    }
+}
