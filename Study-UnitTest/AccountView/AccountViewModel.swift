210505a764914cd8f63af0b676a93aa8065f89e2
WalterLDJunior <walter.junior@meupag.com.br>
2020-07-05 22:20:49 -0300

    Adicionando teste de Protocol

--- Study-UnitTest/AccountView/AccountViewModel.swift
+++ Study-UnitTest/AccountView/AccountViewModel.swift
@@ -34,7 +34,6 @@ class AccountViewModel: AccountViewModelProtocol {
             askUserPermission()
             completion()
         }
-        
         showAlert()
         completion()
     }
---
+++ Study-UnitTestTests/AccountViewModelSpecs.swift
@@ -0,0 +1,53 @@
+//
+//  AccountViewModelSpecs.swift
+//  Study-UnitTestTests
+//
+//  Created by Walter Luis Deonísio Junior on 05/07/20.
+//  Copyright © 2020 Walter Luis Deonísio Junior. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Study_UnitTest
+
+class AccountViewModelSpecs: QuickSpec {
+    override func spec() {
+        describe("AccountViewModel") {
+            var sut: AccountViewModel!
+            
+            context("Should Request Location") {
+                it("Use Location True") {
+                    sut = AccountViewModel(shouldUserLocation: true)
+                    
+                    var showAlert = false
+                    var askUserPermission = false
+                    var completion = false
+                    
+                    sut.shouldRequestLocation(showAlert: {
+                        showAlert = true
+                    }, askUserPermission: {
+                        askUserPermission = true
+                    }, completion: {
+                        completion = true
+                    })
+                    
+                    expect(showAlert).to(equal(true))
+                    expect(askUserPermission).to(equal(true))
+                    expect(completion).to(equal(true))
+                }
+                
+                it("LoginButtonTap") {
+                    sut = AccountViewModel(shouldUserLocation: false)
+                    expect(sut.loginButtonTap()).to(beVoid())
+                }
+            }
+            
+            context("AccountViewModel Protocol") {
+                it("Verify AccountViewModelProtocol"){
+                    sut = AccountViewModel(shouldUserLocation: false)
+                    expect(sut).to(beAKindOf(AccountViewModelProtocol.self))
+                }
+            }
+        }
+    }
+}
